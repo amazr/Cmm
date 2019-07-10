5bd2801c8098ac19f48203ca64f3d472ef0bd728
bouldercoder9 <alma9011@colorado.edu>
2019-07-09 18:31:41 -0600

    wkjefnkj

--- CmmI.cpp
+++ CmmI.cpp
@@ -7,10 +7,81 @@
 
 class variables {
 public:
-	variables(std::string n, std::string t, int loc) {
+	variables(std::string n, std::string t, int loc, std::string value) {
 		name = n;
 		type = t;
 		location = loc;
+		setValue(value);
+	}
+
+	void setValue(std::string value) {
+		//INTEGER
+		if (type == datatypes.at(0)) {
+			try {
+				iValue = stoi(value);
+				valueString = std::to_string(iValue);
+			}
+			catch (std::out_of_range) {
+				//create warning
+			}
+			catch (std::invalid_argument) {
+				//create warning
+			}
+
+		}
+		//DECIMAL
+		else if (type == datatypes.at(1)) {
+			try {
+				dValue = stod(value);
+				valueString = std::to_string(dValue);
+				int precision = 0;
+				for (int i = 0; i < valueString.size(); i++) {
+					if (valueString[i] != '0') {
+						precision = i;
+					}
+				}
+
+				valueString.erase(valueString.begin() + precision + 1, valueString.end());
+			}
+			catch (std::out_of_range) {
+				//create warning
+			}
+			catch (std::invalid_argument) {
+				//create warning
+			}
+		}
+		//CHAR
+		else if (type == datatypes.at(2)) {
+			cValue = value[0];
+			valueString += cValue;
+			if (value.size() > 1) {
+				//Create warning
+			}
+		}
+		//BOOL
+		else if (type == datatypes.at(3)) {
+			if (value == "true" || value == "1") {
+				bValue = true;
+				valueString = "true";
+			}
+			else if (value == "false" || value == "0") {
+				bValue = false;
+				valueString = "false";
+			}
+			else {
+				//create warning
+			}
+		}
+		//STRING
+		else if (type == datatypes.at(4)) {
+			sValue = value;
+			valueString = sValue;
+		}
+
+	}
+	
+	std::string getValueString() {
+		return valueString;
 	}
 
 	std::string getName() {
@@ -26,9 +97,16 @@ public:
 	}
 
 private:
+	std::vector<std::string> datatypes = { "int", "dec", "chr", "bol", "str" };
 	std::string name;
 	std::string type;
 	int location;
+	int iValue;
+	double dValue;
+	char cValue;
+	bool bValue;
+	std::string sValue;
+	std::string valueString = "";
 };
 
 struct line {
@@ -53,17 +131,26 @@ int main() {
 
 	std::unordered_map<std::string, int> var_map;
 	std::vector<variables> var_vector;
-	var_vector.push_back(variables("numOfDogs", "int", var_vector.size()));
-	var_map[var_vector.at(0).getName()] = var_vector.at(0).getLocation();
+	int numOfVars = var_vector.size();
+
+	var_vector.push_back(variables("numOfDogs", "int", numOfVars, "4"));
+	var_map[var_vector.at(numOfVars).getName()] = var_vector.at(numOfVars).getLocation();
+	numOfVars = var_vector.size();
+
+	var_vector.push_back(variables("numOfCats", "dec", numOfVars, "12.000020"));
+	var_map[var_vector.at(numOfVars).getName()] = var_vector.at(numOfVars).getLocation();
+	numOfVars = var_vector.size();
+
+	var_vector.push_back(variables("numOfBirds", "chr", numOfVars, "83"));
+	var_map[var_vector.at(numOfVars).getName()] = var_vector.at(numOfVars).getLocation();
+	numOfVars = var_vector.size();
+
+	var_vector.push_back(variables("doYouHaveAName", "bol", numOfVars, "true"));
+	var_map[var_vector.at(numOfVars).getName()] = var_vector.at(numOfVars).getLocation();
+	numOfVars = var_vector.size();
 
 	for (auto elem : var_vector) {
-		std::cout << "Name: " << elem.getName() << "\tType: " << elem.getType();
-	}
-
-	std::cout << std::endl;
-
-	for (auto elem : var_map) {
-		std::cout << "Name: " << elem.first << "\tLocation: " << elem.second;
+		std::cout << "Name: " << elem.getName() << "\t\t\tType: " << elem.getType() << "\tValue: " << elem.getValueString() << std::endl;
 	}
 
 	std::cout << std::endl;
